a3fe1300c9daa465dd3b6a4275365de5c0741811
Selokod <selosel1907@gmail.com>
2023-04-21 00:43:41 +0300

    ljkvlkv çljkhvl

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.Spring;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String klm ="bunuyazıncakalem" ; 
+System.out.println(klm);
+
+
+    }
+}
